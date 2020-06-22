16a11e53cde3d87fb6eee59b6764e1d944725eff
Hakan Kayra Doğan <hakankayradogan@gmail.com>
2020-06-23 00:06:58 +0300

    :recycle:

--- modules/help.js
+++ modules/help.js
@@ -3,7 +3,7 @@ module.exports = {
     description: "Prints the help menu.",
     help(message) {
         message.channel.send(
-            "```!help```: Prints out this menu. \n ```!kick```: Kicks the mentioned user. \n ```!ban```: Bans the mentioned user. \n ```!clear```: Clears the given amount of messages."
+            "```!help: Prints out this menu. \n !kick: Kicks the mentioned user. \n !ban: Bans the mentioned user. \n !clear: Clears the given amount of messages.```"
         );
     }
 }
